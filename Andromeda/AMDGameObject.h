2c83bbe062478865701b0117ce357c9e018bf3d3
Jos Kuijpers <jos@kuijpersvof.nl>
2014-04-14 22:30:04 +0200

    Organize Source folder. Add Game object class.

---
+++ Andromeda/AMDGameObject.h
@@ -0,0 +1,13 @@
+//
+//  AMDGameObject.h
+//  Andromeda
+//
+//  Created by Jos Kuijpers on 14/04/14.
+//  Copyright (c) 2014 Jarvix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMDGameObject : NSObject
+
+@end
---
+++ Andromeda/AMDGameObject.m
@@ -0,0 +1,13 @@
+//
+//  AMDGameObject.m
+//  Andromeda
+//
+//  Created by Jos Kuijpers on 14/04/14.
+//  Copyright (c) 2014 Jarvix. All rights reserved.
+//
+
+#import "AMDGameObject.h"
+
+@implementation AMDGameObject
+
+@end
